b614c01baa48b83e89185be6fc37925bd3332ae4
Zhang_Xu <axhuoga1203@163.com>
2025-02-27 12:44:12 +0000

    add new code

---
+++ zhengfangti.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+     double n,s;
+    for(;;){
+        printf("请输入一个正方体的边长:\n");
+        cin>>n;
+        s=n*n*6;
+         cout<<s<<endl;
+    }
+    return 0;
+}
