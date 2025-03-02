0d0ded7d444ac801d411f2b0bd075ef10bf3f93a
Zhang_Xu <axhuoga1203@163.com>
2025-03-02 02:58:19 +0000

    add new code

--- k.cpp
+++ k.cpp
@@ -3,14 +3,19 @@ using namespace std;
 int main(){
     int n,s=0;
     cin>>n;
-    s=n%3;
+    s=n%5;
     if(s==0){
         cout<<n<<endl;
     }
     else if(s==1){
         cout<<n-1<<endl;
+    }else if(s==2){
+        cout<<n-2<<endl;
+    }
+    else if(s==3){
+        cout<<n+2<<endl;
     }else{
-        cout<<n+1<<endl;
+         cout<<n+1<<endl;   //s==4
     }
     return 0;
 }
